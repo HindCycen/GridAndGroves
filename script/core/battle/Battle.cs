95ad0ca6022eb4f6d67a5ed0fb85ec67664c7952
HindCycen <sigmacyc@outlook.com>
2026-02-27 18:28:19 +0800

    fix: some motherfucking typos

--- script/core/battle/Battle.cs
+++ script/core/battle/Battle.cs
@@ -4,12 +4,14 @@ using System;
 public partial class Battle : Node2D {
     public override void _Ready() {
         _initializeBlocks();
+        Global.InitRng();
+        Global.InitGrids();
         Global.GetBlock("ExampleBlock", new Vector2(100, 100), this);
-        Global.GetBlock("ExampleMovingRight", new Vector2(200, 200), this);
+        Global.GetBlock("ExampleMoveRight", new Vector2(200, 200), this);
     }
 
     private void _initializeBlocks() {
         Global.SubscribeBlockDef(GD.Load<BlockDef>("res://resources/blockdefs/ExampleBlock.tres"));
-        Global.SubscribeBlockDef(GD.Load<BlockDef>("res://resources/blockdefs/ExampleMovingRight.tres"));
+        Global.SubscribeBlockDef(GD.Load<BlockDef>("res://resources/blockdefs/ExampleMoveRight.tres"));
     }
 }
