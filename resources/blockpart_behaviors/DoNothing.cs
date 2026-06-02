ce4786471c021cff23c824026cb07bf228889a81
HindCycen <sigmacyc@outlook.com>
2026-06-02 12:19:58 +0800

    feat: 添加多个 BlockPart 和 BlockDef 资源，增强游戏功能

---
+++ resources/blockpart_behaviors/DoNothing.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+public partial class DoNothing : BlockPartBehavior {
+    public override void Execute(Block block, BlockPart part) {
+        // Do NOTHING. Just as-is.
+    }
+}
